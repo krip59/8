9f02d1b55d9d927b30999e6a297b2db6630a78c6
Irina_Leskova <krip@mail.ru>
2023-07-06 08:26:24 +0500

    Задания сделаны

---
+++ 1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+using System;
+using static System.Console;
+Clear();
+
+int[] array = GetRandomArray(6, 0, 100);
+Console.WriteLine($"[{String.Join(",",array)}]");
+Console.WriteLine($"Сумма нечетных элементов в массиве = {GetSumUneven(array)}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+int [] result = new int [size];
+for (int i = 0; i < size; i++)
+{
+result [i] = new Random(). Next(minValue, maxValue);
+}
+
+    return result;
+}
+
+int GetSumUneven(int[] array)
+{
+    int sum = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (j % 2 != 0) sum += array[j];
+    }
+    return sum;
+}
---
+++ 58/Program.cs
@@ -0,0 +1,71 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+WriteLine($"Строка с наименьшей суммой - {GetRowNumber(array)}");
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+int GetRowNumber (int [,] array)
+{
+    int row = 0;
+    int minsum = 0;
+    for (int i = 0; i<array.GetLength(1); i++)
+    {
+        minsum = minsum + array[0,i];
+    }
+
+    for (int i = 1; i<array.GetLength(0); i++)
+    {
+        int sum = 0; 
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            sum = sum+array[i,j];
+        }
+        if (minsum > sum)
+        {
+            minsum = sum;
+            row = i;
+        }
+    }
+    return row;
+}
